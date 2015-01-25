c899aca369f8ddce7c547f2c6ea87ac9e0a3d55f
Brian Shumate <brian@brianshumate.com>
2015-01-25 12:00:48 -0500

    new and improved

---
+++ README.md
@@ -0,0 +1,89 @@
+
+```
+				 ____       _             ____  _           _
+				|  _ \ ___ | |__   ___   |  _ \(_)_ __ __ _| |_ ___
+				| |_) / _ \| '_ \ / _ \  | |_) | | '__/ _` | __/ _ \
+				|  _ < (_) | |_) | (_) | |  __/| | | | (_| | ||  __/
+				|_| \_\___/|_.__/ \___/  |_|   |_|_|  \__,_|\__\___|
+
+                       .ed"""" """$$$$be.
+                     -"           ^""**$$$e.
+                   ."                   '$$$c
+                  /                      "4$$b
+                 d  3                     $$$$
+                 $  *                   .$$$$$$
+                .$  ^c           $$$$$e$$$$$$$$.
+                d$L  4.         4$$$$$$$$$$$$$$b
+                $$$$b ^ceeeee.  4$$ECL.F*$$$$$$$
+    e$""=.      $$$$P d$$$$F $ $$$$$$$$$- $$$$$$
+   z$$b. ^c     3$$$F "$$$$b   $"$$$$$$$  $$$$*"      .=""$c
+  4$$$$L   \     $$P"  "$$b   .$ $$$$$...e$$        .=  e$$$.
+  ^*$$$$$c  %..   *c    ..    $$ 3$$$$$$$$$$eF     zP  d$$$$$
+    "**$$$ec   "\   %ce""    $$$  $$$$$$$$$$*    .r" =$$$$P""
+          "*$b.  "c  *$e.    *** d$$$$$"L$$    .d"  e$$***"
+            ^*$$c ^$c $$$      4J$$$$$% $$$ .e*".eeP"
+               "$$$$$$"'$=e....$*$$**$cz$$" "..d$*"
+                 "*$$$  *=%4.$ L L$ P3$$$F $$$P"
+                    "$   "%*ebJLzb$e$$$$$b $P"
+                      %..      4$$$$$$$$$$ "
+                       $$$e   z$$$$$$$$$$%
+                        "*$c  "$$$$$$$P"
+                         ."""*$$$$$$$$bc
+                      .-"    .$***$$$"""*e.
+                   .-"    .e$"     "*$c  ^*b.
+            .=*""""    .e$*"          "*bc  "*$e..
+          .$"        .z*"               ^*$e.   "*****e.
+          $$ee$c   .d"                     "*$.        3.
+          ^*$E")$..$"                         *   .ee==d%
+             $.d$$$*                           *  J$$$e*
+              """""                             "$$$"   Gilo95'
+
+
+```
+
+This is the source for [@robo_pirate](https://twitter.com/robo_pirate/with_replies), a Twitter
+bot whose one sole purpose is to insult Twitter users in a particularly
+pirate-y manner.
+
+## Setup
+
+### Note
+
+Replace all instances of *robo_pirate* in this project with the name of
+your own bot to begin making use of this code for yourself.
+
+Use `virtualenv` and `virtualenvwrapper` because Python is more fun with them.
+
+Clone this repository, the change into its top level directory:
+
+```
+git clone https://github.com/brianshumate/robo_pirate.git
+cd robo_pirate
+```
+
+Then, to quote the venerable @densoneold,
+
+> commands
+
+```
+mkvirtualenv robo_pirate
+pip install -r requirements.txt
+cp robo_pirate/etc/access.json-dist robo_pirate/etc/access.json
+```
+
+Finally, edit `etc/access.json` and plug in your Twitter API keys, tokens,
+and secrets.
+
+## Usage
+
+Run it like this:
+
+```
+./robo_pirate.py
+```
+
+## Thank You
+
+- [Brent Woodruff](https://twitter.com/fprimex): Long time friend, Twitter bot collaborator and influence
+- [@thricedotted](https://twitter.com/thricedotted): For their cool [twitterbot](https://github.com/thricedotted/twitterbot) framework that Robo Pirate uses! <3
+- [Darius Kazemi](https://twitter.com/tinysubversions): Twitter bot artiste extraordinaire and constant inspiration
--- robo_pirate.py
+++ robo_pirate.py
@@ -1,152 +1,167 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-robo_pirate.py
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*- #
 
-Robo pirate is a Twitter bot that is a harmony of code being primarily
-written by [Brent Woodruff](http://www.brentwoodruff.com/) and vocabulary
-plus bits of code written by [Brian Shumate](http://brianshumate.com/).
-
-The aim of Robo Pirate is to insult tweeps in piratey ways.
-
-Copyright (c) 2010 Brian Shumate
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-"""
+import json
 import sys
 import re
 import random
-import urllib2
-import cPickle as pickle
-import twitter
-from sets import Set
+from twitterbot import TwitterBot
 
-random.seed()
 
-consumer_key = 'OAUTH CONSUMER KEY HERE'
-consumer_secret = 'OAUTH CONSUMER SECRET HERE'
-access_token = 'OAUTH ACCESS TOKEN HERE'
-access_token_secret = 'OAUTH ACCESS TOKEN SECRET HERE'
-re_fu = re.compile("^.*fuck[\s|!|.]*you.*$", flags=re.I)
-store_filename = './tweets.store'
-username = 'TWITTER USERNAME HERE'
+class RoboPirate(TwitterBot):
 
-def get_insult():
-    starters = ['Ahoy! Ye be', 'Arrr! Thar be', 'Avast! Ye be', 'Avast! Ye be more foul \'an', 'Avast! Thar be', 'Avast! Ye be resemblin\'', 'Aye! No doubt ye be', 'By my reckonin\' ye be', 'Blimey! Ye \'ppear t\' be', 'Come now, fer ye be', 'D\'ye see?! Ye must be', 'Sail ho! Yer filthier \'an', 'Scupper that \'an begone! Ye be', 'Holloa! Ye be', 'How are \'ee?! Ye be', 'Shiver me timbers! Ye be', 'Show a leg! Fer ye surely be', 'Sink me! Ye be', 'Stay yer tears! Ye be', 'Stint yer clack! Ye be', 'Stop yer clapper! Ye be', 'Yo ho! Ye be', 'Yo ho! Thar be', 'Yarrr! Ye be filthier \'an', 'Blimey! Ye be filthier \'an', 'Avast! Ye be filthier \'an', 'Yo ho! Ye be filthier \'an', 'Avast! Ye be nastier \'an', 'Yarrr! Ye be nastier \'an', 'Yo ho! Ye be nastier \'an', 'Blimey! Ye be nastier \'an', 'Yo ho ho! Ye be', 'Aye! Ye be!', 'Ye be remindin\' me of',]
-    nouns = ['backsides', 'barkadeers', 'bilge waters', 'ballast pigs', 'barnacles', 'bowsprits', 'belayin\' pins', 'bilge drinkers', 'bilge rats', 'black spots', 'blaggards',  'blowfishes',  'boatswains', 'boat hooks', 'bones', 'buccaneers', 'butts', 'buttles', 'buttcabbages', 'buttnapkinsmythes', 'buttankards', 'buttossacks', 'buttards', 'cabin boys', 'carousers', 'castaways', 'cat o\' nine tails', 'chandlers', 'cogs', 'curs', 'cusses', 'dandies',  'deadlights', 'doxies', 'fish gizzards' 'freebooters', 'Frenchmen',  'futtocks', 'gangplanks', 'gibbits', 'gibbit cages', 'gizzards', 'grog blossoms', 'hawseholes', 'hempen jigs', 'hempen halters', 'hogsheads', 'holystones', 'horn swoggler', 'hulks', 'humdingers', 'interlopers', 'Jack Ketches', 'knaves', 'killicks', 'lads', 'luggers', 'lynch baits', 'poop decks', 'jim lads', 'krakens', 'landlubbers', 'lasses', 'lassies', 'lassie-lenders', 'lubbers', 'marroons', 'parrots', 'petards', 'picaroons', 'powder chests', 'peglegs', 'powder monkeys', 'pressganngs', 'privateers', 'private parts', 'privy parts',  'rapscallions', 'rope burns', 'sandcrabs', 'sassielassies', 'saw bones', 'scallywags', 'scoundrels', 'scurvy dogs', 'sea lice', 'six pounders', 'smythes', 'smythesackers', 'smythesluggers', 'smytheslorpers', 'spankers', 'strike colors', 'sea bass', 'sea dogs', 'sea witches', 'scuttle hounds', 'sluggards', 'sunken skulls', 'strumpets', 'swabbies',  'stumps', 'swashers', 'swine', 'tar stains', 'tarts', 'tarty mermaids', 'twitterfools', 'tweeples', 'tweeps', 'twenches', 'twittles', 'twittlesmiths', 'twittlesmythes', 'twits', 'wenches', 'wherries',
-        ]
-    amounts = ['barrel', 'cask', 'chest', 'draught', 'fathom', 'full hold', 'hull full',  'keg', 'keg full', 'league', 'motherload', 'tankard', 'stein', 'sea chest',]
-    adjectives = ['addle brained', 'barnacle bottomed', 'becalmed', 'bedraggled', 'b\'tween decks', 'black eyed', 'black toothed', 'blood thirsty', 'barnacled', 'barrel belly\'d', 'black hearted', 'blubberin\'', 'bow legged', 'brine crusted', 'chicken hearted', 'confounded', 'crows footed', 'drunken', 'flea bitten', 'floggish', 'fog bound', 'furlish', 'gibbitly', 'gibbitish', 'goutish', 'gout-toed', 'green gilled', 'green limbed', 'groggy', 'grog brained', 'grog headed', 'groggish', 'grog swiggin\'', 'grog snarfin\'', 'handsomely', 'hoggish', 'hornswagglish', 'horn swogglin\'', 'hulkish', 'knock kneed', 'lead slingin\'',  'lice infested', 'lily livered', 'lily sniffin\'', 'luggish', 'marroonish', 'mast huggin\'', 'one armed', 'one legged', 'one toed', 'peglegged', 'pigeon toed', 'parrot lubbin\'', 'plunderin\'', 'plunderish', 'powder wettin\'', 'pox faced', 'pox infested', 'rot mouth\'d', 'scrappy', 'scrappish', 'keelhaulish', 'motherload-ish', 'mutinous', 'rum swiggin\'', 'salt crusted', 'scutt\'ly', 'scuttlish', 'scurvish', 'scurvy', 'skirt wearin\'', 'square-rigged', 'squiffy', 'stinkin\'', 'swabbish', 'swaggardly', 'swiney', 'tar stain\'d', 'thunderin\'', 'tweetish', 'twittish', 'wind blown', 'worm infested', 'worm riddled', 'yellow', 'yellow-bell\'yd',]
-    starter = starters[random.randint(0, len(starters) - 1)]
-    adj1 = adjectives[random.randint(0, len(adjectives) - 1)]
-    adj2 = adjectives[random.randint(0, len(adjectives) - 1)]
-    noun = nouns[random.randint(0, len(nouns) - 1)]
-    amount = amounts[random.randint(0, len(amounts) - 1)]
-    if adj1 == adj2:
+    def bot_init(self):
+        """
+        Initialize and configure your bot!
+
+        Use this function to set options and initialize your own custom bot
+        state (if any).
+        """
+
+        ############################
+        # REQUIRED: LOGIN DETAILS! #
+        ############################
+
+        with open("etc/access.json", 'r') as access:
+            authdb = json.load(access)
+
+        self.config['api_key'] = authdb["API_Key"]
+        self.config['api_secret'] = authdb["API_Secret"]
+        self.config['access_key'] = authdb["Access_Token"]
+        self.config['access_secret'] = authdb["Access_Token_Secret"]
+
+        access.close()
+
+        ######################################
+        # SEMI-OPTIONAL: OTHER CONFIG STUFF! #
+        ######################################
+
+        # how often to tweet, in seconds
+        self.config['tweet_interval'] = 15 * 60     # default: 30 minutes
+
+        # use this to define a (min, max) random range of how often to tweet
+        # e.g., self.config['tweet_interval_range'] = (5*60, 10*60) # tweets every 5-10 minutes
+        self.config['tweet_interval_range'] = None
+
+        # only reply to tweets that specifically mention the bot
+        self.config['reply_direct_mention_only'] = False
+
+        # only include bot followers (and original tweeter) in @-replies
+        self.config['reply_followers_only'] = True
+
+        # fav any tweets that mention this bot?
+        self.config['autofav_mentions'] = False
+
+        # fav any tweets containing these keywords?
+        self.config['autofav_keywords'] = []
+
+        # follow back all followers?
+        self.config['autofollow'] = False
+
+
+        ###########################################
+        # CUSTOM: your bot's own state variables! #
+        ###########################################
+
+        # If you'd like to save variables with the bot's state, use the
+        # self.state dictionary. These will only be initialized if the bot is
+        # not loading a previous saved state.
+
+        # self.state['butt_counter'] = 0
+
+        # You can also add custom functions that run at regular intervals
+        # using self.register_custom_handler(function, interval).
+        #
+        # For instance, if your normal timeline tweet interval is every 30
+        # minutes, but you'd also like to post something different every 24
+        # hours, you would implement self.my_function and add the following
+        # line here:
+
+        # self.register_custom_handler(self.my_function, 60 * 60 * 24)
+
+    def get_insult(wat):
+        # print wat
+        starters = ['Ahoy! Ye be', 'Arrr! Thar be', 'Avast! Ye be', 'Avast! Ye be more foul \'an', 'Avast! Thar be', 'Avast! Ye be resemblin\'', 'Aye! No doubt ye be', 'By my reckonin\' ye be', 'Blimey! Ye \'ppear t\' be', 'Come now, fer ye be', 'D\'ye see?! Ye must be', 'Sail ho! Yer filthier \'an', 'Scupper that \'an begone! Ye be', 'Holloa! Ye be', 'How are \'ee?! Ye be', 'Shiver me timbers! Ye be', 'Show a leg! Fer ye surely be', 'Sink me! Ye be', 'Stay yer tears! Ye be', 'Stint yer clack! Ye be', 'Stop yer clapper! Ye be', 'Yo ho! Ye be', 'Yo ho! Thar be', 'Yarrr! Ye be filthier \'an', 'Blimey! Ye be filthier \'an', 'Avast! Ye be filthier \'an', 'Yo ho! Ye be filthier \'an', 'Avast! Ye be nastier \'an', 'Yarrr! Ye be nastier \'an', 'Yo ho! Ye be nastier \'an', 'Blimey! Ye be nastier \'an', 'Yo ho ho! Ye be', 'Aye! Ye be!', 'Ye be remindin\' me of',]
+        nouns = ['backsides', 'barkadeers', 'bilge waters', 'ballast pigs', 'barnacles', 'bowsprits', 'belayin\' pins', 'bilge drinkers', 'bilge rats', 'black spots', 'blaggards',  'blowfishes',  'boatswains', 'boat hooks', 'bones', 'buccaneers', 'butts', 'buttles', 'buttcabbages', 'buttnapkinsmythes', 'buttankards', 'buttossacks', 'buttards', 'cabin boys', 'carousers', 'castaways', 'cat o\' nine tails', 'chandlers', 'cogs', 'curs', 'cusses', 'dandies',  'deadlights', 'doxies', 'fish gizzards' 'freebooters', 'Frenchmen',  'futtocks', 'gangplanks', 'gibbits', 'gibbit cages', 'gizzards', 'grog blossoms', 'hawseholes', 'hempen jigs', 'hempen halters', 'hogsheads', 'holystones', 'horn swoggler', 'hulks', 'humdingers', 'interlopers', 'Jack Ketches', 'knaves', 'killicks', 'lads', 'luggers', 'lynch baits', 'poop decks', 'jim lads', 'krakens', 'landlubbers', 'lasses', 'lassies', 'lassie-lenders', 'lubbers', 'marroons', 'parrots', 'petards', 'picaroons', 'powder chests', 'peglegs', 'powder monkeys', 'pressganngs', 'privateers', 'private parts', 'privy parts',  'rapscallions', 'rope burns', 'sandcrabs', 'sassielassies', 'saw bones', 'scallywags', 'scoundrels', 'scurvy dogs', 'sea lice', 'six pounders', 'smythes', 'smythesackers', 'smythesluggers', 'smytheslorpers', 'spankers', 'strike colors', 'sea bass', 'sea dogs', 'sea witches', 'scuttle hounds', 'sluggards', 'sunken skulls', 'strumpets', 'swabbies',  'stumps', 'swashers', 'swine', 'tar stains', 'tarts', 'tarty mermaids', 'twitterfools', 'tweeples', 'tweeps', 'twenches', 'twittles', 'twittlesmiths', 'twittlesmythes', 'twits', 'wenches', 'wherries',
+            ]
+        amounts = ['barrel', 'cask', 'chest', 'draught', 'fathom', 'full hold', 'hull full',  'keg', 'keg full', 'league', 'motherload', 'tankard', 'stein', 'sea chest',]
+        adjectives = ['addle brained', 'barnacle bottomed', 'becalmed', 'bedraggled', 'b\'tween decks', 'black eyed', 'black toothed', 'blood thirsty', 'barnacled', 'barrel belly\'d', 'black hearted', 'blubberin\'', 'bow legged', 'brine crusted', 'chicken hearted', 'confounded', 'crows footed', 'drunken', 'flea bitten', 'floggish', 'fog bound', 'furlish', 'gibbitly', 'gibbitish', 'goutish', 'gout-toed', 'green gilled', 'green limbed', 'groggy', 'grog brained', 'grog headed', 'groggish', 'grog swiggin\'', 'grog snarfin\'', 'handsomely', 'hoggish', 'hornswagglish', 'horn swogglin\'', 'hulkish', 'knock kneed', 'lead slingin\'',  'lice infested', 'lily livered', 'lily sniffin\'', 'luggish', 'marroonish', 'mast huggin\'', 'one armed', 'one legged', 'one toed', 'peglegged', 'pigeon toed', 'parrot lubbin\'', 'plunderin\'', 'plunderish', 'powder wettin\'', 'pox faced', 'pox infested', 'rot mouth\'d', 'scrappy', 'scrappish', 'keelhaulish', 'motherload-ish', 'mutinous', 'rum swiggin\'', 'salt crusted', 'scutt\'ly', 'scuttlish', 'scurvish', 'scurvy', 'skirt wearin\'', 'square-rigged', 'squiffy', 'stinkin\'', 'swabbish', 'swaggardly', 'swiney', 'tar stain\'d', 'thunderin\'', 'tweetish', 'twittish', 'wind blown', 'worm infested', 'worm riddled', 'yellow', 'yellow-bell\'yd',]
+        starter = starters[random.randint(0, len(starters) - 1)]
+        adj1 = adjectives[random.randint(0, len(adjectives) - 1)]
         adj2 = adjectives[random.randint(0, len(adjectives) - 1)]
-    if not adj1[0] in 'aeiou':
-        an = 'a'
-    else:
-        an = 'an'
-
-    return "%s %s %s %s 'o %s %s." % (starter, an, adj1, amount, adj2, noun)
-
-try:
-    store_file = open(store_filename, 'rb')
-    store = pickle.load(store_file)
-except IOError:
-    # Replying to all since the beginning at this point == NO BUENO!
-    store = { 'last_reply_id':None, 'last_dm_id':None, }
-except pickle.PickleError:
-    sys.exit(1)
-
-api = twitter.Api(consumer_key=consumer_key,
-                    consumer_secret=consumer_secret,
-                    access_token_key=access_token,
-                    access_token_secret=access_token_secret)
-
-following = api.GetFriends()
-friend_names = Set()
-for friend in following:
-    friend_names.add(friend.screen_name)
-
-followers = api.GetFollowers()
-for follower in followers:
-    if (not follower.screen_name in friend_names):
-        try:
-            api.CreateFriendship(follower.screen_name)
-            print "Created friendship with " + follower.screen_name
-        except urllib2.HTTPError:
-            msg = "@" + follower.screen_name + " "
-            msg += get_insult()
-
-for friend in friend_names:
-    print friend
-
-print "Getting replies since: " + repr(store['last_reply_id'])
-replies = api.GetReplies(since_id=store['last_reply_id'])
-for reply in replies:
-    if not reply.user.screen_name == username:
-        if reply.id > store['last_reply_id']:
-            store['last_reply_id'] = reply.id
-        reply_msg = "@" + reply.user.screen_name + " "
-        if re_fu.match(reply.text):
-            reply_msg += "no, FUCK YOU!"
+        noun = nouns[random.randint(0, len(nouns) - 1)]
+        amount = amounts[random.randint(0, len(amounts) - 1)]
+        if adj1 == adj2:
+            adj2 = adjectives[random.randint(0, len(adjectives) - 1)]
+        if not adj1[0] in 'aeiou':
+            an = 'a'
         else:
-            reply_msg += get_insult()
-        api.PostUpdate(reply_msg, in_reply_to_status_id=reply.id)
-        print "Posted reply to " + repr(reply.id) + ":" + reply_msg
+            an = 'an'
 
-print "Getting direct messages since: " + repr(store['last_dm_id'])
-dms = api.GetDirectMessages(since_id=store['last_dm_id'])
-for dm in dms:
-    if not dm.sender_screen_name == username:
-        if dm.id > store['last_dm_id']:
-            store['last_dm_id'] = dm.id
-        msg = " "
-        if re_fu.match(dm.text):
-            msg += "no, FUCK YOU!"
-        else:
-            msg += get_insult()
-        try:
-                api.PostDirectMessage(dm.sender_screen_name, msg)
-        except:
-                print "it blowed up at me!"
-        print "Posted direct message to " + dm.sender_screen_name + ":" + msg
+        return "%s %s %s %s 'o %s %s." % (starter, an, adj1, amount, adj2, noun)
 
-# Get a random friend and insult them
-if len(friend_names) != 0:
-    msg = "@" + random.sample(tuple(friend_names), 1)[0] + " " + get_insult()
-    api.PostUpdate(msg)
-    print "Posted update:" + msg
+    def on_scheduled_tweet(self):
+        """
+        Make a public tweet to the bot's own timeline.
 
-# Uncomment below to bust out the broadsides and insult all followers!
-#for friend in friend_names:
-#    msg = "@" + friend + " " + get_insult()
-#    api.PostUpdate(msg)
-#    print "Posting update:" + msg
+        It's up to you to ensure that it's less than 140 characters.
 
-print store
+        Set tweet frequency in seconds with TWEET_INTERVAL in config.py.
+        """
+        text = self.get_insult()
+        self.post_tweet(text)
 
-store_file = open(store_filename, 'wb')
-try:
-    pickle.dump(store, store_file)
-finally:
-    store_file.close()
+    def on_mention(self, tweet, prefix):
+        """
+        Defines actions to take when a mention is received.
+
+        tweet - a tweepy.Status object. You can access the text with
+        tweet.text
+
+        prefix - the @-mentions for this reply. No need to include this in the
+        reply string; it's provided so you can use it to make sure the value
+        you return is within the 140 character limit with this.
+
+        It's up to you to ensure that the prefix and tweet are less than 140
+        characters.
+
+        When calling post_tweet, you MUST include reply_to=tweet, or
+        Twitter won't count it as a reply.
+        """
+        text = self.get_insult()
+        prefixed_text = prefix + ' ' + text
+        self.post_tweet(prefix + ' ' + text, reply_to=tweet)
+
+        # call this to fav the tweet!
+        # if something:
+        #     self.favorite_tweet(tweet)
+
+    def on_timeline(self, tweet, prefix):
+        """
+        Defines actions to take on a timeline tweet.
+
+        tweet - a tweepy.Status object. You can access the text with
+        tweet.text
+
+        prefix - the @-mentions for this reply. No need to include this in the
+        reply string; it's provided so you can use it to make sure the value
+        you return is within the 140 character limit with this.
+
+        It's up to you to ensure that the prefix and tweet are less than 140
+        characters.
+
+        When calling post_tweet, you MUST include reply_to=tweet, or
+        Twitter won't count it as a reply.
+        """
+        text = self.get_insult()
+        prefixed_text = prefix + ' ' + text
+        self.post_tweet(prefix + ' ' + text, reply_to=tweet)
+
+        # call this to fav the tweet!
+        # if something:
+        #     self.favorite_tweet(tweet)
+
+
+if __name__ == '__main__':
+    bot = RoboPirate()
+    bot.run()
