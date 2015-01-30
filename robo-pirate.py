fee55234b49c80f4b0d9b29119d1f256e95876ef
Brian Shumate <brian@brianshumate.com>
2015-01-30 11:04:48 -0500

    less often

--- robo-pirate.py
+++ robo-pirate.py
@@ -37,11 +37,13 @@ class RoboPirate(TwitterBot):
         ######################################
 
         # how often to tweet, in seconds
-        self.config['tweet_interval'] = 15 * 60     # 15 minutes
+        self.config['tweet_interval'] = 120 * 60     # 120 minutes
 
         # use this to define a (min, max) random range of how often to tweet
         # e.g., self.config['tweet_interval_range'] = (5*60, 10*60) # tweets every 5-10 minutes
-        self.config['tweet_interval_range'] = None
+
+        # Tweet range: every 120 to 420 minutes
+        self.config['tweet_interval_range'] = (120*60, 420*60)
 
         # only reply to tweets that specifically mention the bot
         self.config['reply_direct_mention_only'] = True
